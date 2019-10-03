3ee6a05a29a14adc9cbb029769d650f8eb341dd7
kouma1990 <kouma1990@gmail.com>
2019-10-03 17:57:20 +0900

    add: google analytics

--- nuxt.config.js
+++ nuxt.config.js
@@ -30,7 +30,7 @@ export default {
   /*
    ** Plugins to load before mounting the App
    */
-  plugins: ['~/plugins/firebase'],
+  plugins: ['~/plugins/firebase', '~plugins/ga.js'],
   /*
    ** Nuxt.js dev-modules
    */
